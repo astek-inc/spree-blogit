e3c4aa2e8cb68a8d9d391382a60c3ddd697093ae
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-20 17:27:06 -0800

    Update original for override

--- app/overrides/add_blog_posts_to_admin_menu.rb
+++ app/overrides/add_blog_posts_to_admin_menu.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(
-    :original => 'c3e21218da38ac0db70bb54059726c3fa153c3bd',
-    :virtual_path => 'spree/layouts/admin',
-    :name => 'add_blog_posts_to_admin_menu',
-    :insert_bottom => '[data-hook="admin_tabs"]',
-    :text => '<ul class="nav nav-sidebar">
-        <%= tab t(:blog_posts, scope: \'blogit.posts\'), :url => admin_blog_root_path, :icon => \'bullhorn\' %>
+    original: '07c30c283cd22d1e6325883763590f916c2e7104',
+    virtual_path: 'spree/layouts/admin',
+    name: 'add_blog_posts_to_admin_menu',
+    insert_bottom: '[data-hook="admin_tabs"]',
+    text: '<ul class="nav nav-sidebar">
+        <%= tab t(:blog_posts, scope: \'blogit.posts\'), url: admin_blog_root_path, icon: \'bullhorn\' %>
       </ul>'
 )
